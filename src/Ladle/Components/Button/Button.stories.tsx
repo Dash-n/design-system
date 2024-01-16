56bf3b2fcd018784616c697c2c255c1805cb40c2
Chris Chung <37494613+chrisjhchung@users.noreply.github.com>
2024-01-16 17:25:20 +1100

    chore: add styles example

--- src/Ladle/Components/Button/Button.stories.tsx
+++ src/Ladle/Components/Button/Button.stories.tsx
@@ -1,9 +1,14 @@
 import type { Story } from "@ladle/react";
+import styles from './Button.module.css';
 
 type Props = { label: string };
 
 export const Button: Story<Props> = ({ label }) => (
-  <button>{label ?? "Button"}</button>
+  <button
+    className={styles.button}
+  >
+    {label ?? "Button"}
+  </button>
 );
 Button.args = {
   label: "Test",
