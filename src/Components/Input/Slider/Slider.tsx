dc789cfae30e21cd7375a7d8bc8092472f4d4a4a
donathany <donathany94@gmail.com>
2024-02-01 22:31:47 +0800

    react-slider
    redid the whole slider component with react-slider

--- src/Components/Input/Slider/Slider.tsx
+++ src/Components/Input/Slider/Slider.tsx
@@ -1,107 +1,57 @@
 import type { Story } from "@ladle/react";
 import styles from "./Slider.module.css";
-import { useRef, useState, useEffect } from "react";
+import ReactSlider from "react-slider";
 
 type Props = {
   // id: string;
   // name: string;
-  value: number;
   disabled?: boolean;
   min: number;
   max: number;
   step: number;
+  value: number;
   setValue: (e: number) => void;
 };
 
 // const value = useRef(0)
 // const [value, setValue] = useState("");
 export const Slider: Story<Props> = ({ max, min, step, value, setValue }) => {
-  const ref = useRef(null);
-
-  const divStyle = {
-    width: "50px",
-    height: "50px",
-    backgroundColor: "#3498db",
-    position: "relative",
-    transform: `translateX(${value}px)`,
-    transition: "transform 0.2s ease-in-out",
-    marginBottom: "20px", // Add margin to create space for the tooltip
-  };
-
-  const tooltipStyle = {
+  const toolTip = {
     position: "absolute",
-    top: "-50px",
-    marginLeft: `calc(${(98 / max) * value}% + 1% - 6px - ${2 * value}px)`,
-    // padding: "0 12px",
-    height: "24px",
-    width: "24px", // Set width to 'auto' to adjust based on content
-    borderRadius: "16px 16px 0 16px",
-    transform: "rotate(45deg)",
+    display: "flex",
+    justifyContent: "center",
 
-    textAlign: "center",
-    backgroundColor: "#4f84f7",
-    color: "#fff",
-    padding: "5px",
-    // borderRadius: "5px",
-    display: "inline-block",
+    width: "var(--tooltipWidth)",
+    height: "var(--tooltipWidth)",
+    borderRadius: "50% 50% 50% 0",
+    transform: "translateY(calc(var(--tooltipWidth) * -1.25)) rotate(-45deg)",
+    background: "#4F84F7",
+    color: "white",
+    content: "",
   };
 
   return (
     <div>
-      <div className={styles.slidecontainer}>
-        <div className="labelLeft">{min}</div>
-        <div
-          style={{
-            position: "relative",
-            display: "flex",
-            width: "100%",
-            padding: "0",
-            // border: "2px green solid",
-          }}
-        >
-          <input
-            className={styles.slider}
-            ref={ref}
-            type="range"
-            min={min}
-            max={max}
-            step={step}
-            value={value}
-            list="tickmarks"
-            onChange={(e) => {
-              setValue(e.target.value);
-              // ref.current.left = value;
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              width: "100%",
-              padding: "0 4px",
-              position: "relative",
-            }}
-          >
-            <div style={tooltipStyle}>
-              <div className={styles.tooltipText}>{value}</div>
+      <ReactSlider
+        className={styles.horizontalSlider}
+        marks={step}
+        markClassName={styles.mark}
+        min={min}
+        max={max}
+        step={step}
+        thumbClassName={styles.thumb}
+        trackClassName={styles.track}
+        value={value}
+        onChange={(value, index) => setValue(value)}
+        renderThumb={(props, state) => (
+          <div {...props}>
+            <div style={toolTip}>
+              <div style={{ transform: "rotate(45deg)" }}>{state.valueNow}</div>
             </div>
-            {Array.from({ length: (max - min) / step + 1 }, (_, index) => (
-              <span
-                style={{
-                  height: "16px",
-                  width: "16px",
-                  borderRadius: "50%",
-                  background: "#4f84f7",
-                  userSelect: "none",
-                  zIndex: -1,
-                }}
-              />
-            ))}
           </div>
-        </div>
-        <div className="labelRight">{max}</div>
-      </div>
+        )}
+      />
+      {/* <div>{value}</div> */}
     </div>
   );
 };
--- src/Ladle/Components/Input/Slider.stories.tsx
+++ src/Ladle/Components/Input/Slider.stories.tsx
@@ -1,22 +1,21 @@
 import type { Story } from "@ladle/react";
 import { Slider } from "../../../Components/Input/Slider/Slider.tsx";
 import { useState, useEffect } from "react";
+import ReactSlider from "react-slider";
 
 type Props = {
   options: string;
 };
 
 export const Sliders: Story<Props> = ({}) => {
-  const [value, setValue] = useState(5);
+  const [value, setValue] = useState(20);
 
   // const [leftpos, setLeftpos] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    setValue(3);
-  }, []);
   return (
     <div>
-      <Slider min={0} max={10} step={1} value={value} setValue={setValue} />
+      <Slider min={0} max={100} step={10} value={value} setValue={setValue} />
+      Value is: {value}
     </div>
   );
 };
