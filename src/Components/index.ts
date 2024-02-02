6fa737aa86a5631d2e7053e29be303e39b3b43b3
donathany <donathany94@gmail.com>
2024-02-02 16:22:30 +0800

    Update index.ts
    removed comment

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,5 +2,4 @@ export const toTitlecase = (label: string) => {
   return label.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
   });
-  //   return label.substring(0, 1).toUpperCase() + label.substring(1);
 };
