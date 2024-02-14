0cbe66ad6842ea172a18f9ce8e6040e7613b6a29
donathany <61956007+donathany@users.noreply.github.com>
2024-02-14 18:36:25 +0800

    input initial commit (#2)
    * input initial commit
    
    Checkbox, Radio, Text inputs
    
    * slider initial commit
    
    slider initial commit + input resolutions
    
    * moved files
    
    moved Ladle/Inputs folder into Ladle/Components
    
    * working commit
    
    commit of radio, checkbox, and slider in a working state, with binded labels to check
    
    * slider update
    
    missing slider alignment, needs cleaning up
    
    * slider code-only
    
    90% done, math still not quite there
    
    * react-slider
    
    redid the whole slider component with react-slider
    
    * resolution commit
    
    moved ladle input stories out of ladle/component folder, resolved most of PR comments, still need to check radio component

---
+++ src/Components/Input/Slider/Slider.tsx
@@ -0,0 +1,42 @@
+import type { Story } from "@ladle/react";
+import styles from "./Slider.module.css";
+import ReactSlider from "react-slider";
+
+type Props = {
+  id: string;
+  name: string;
+  disabled?: boolean;
+  min: number;
+  max: number;
+  step: number;
+  value: number;
+  setValue: (e: number) => void;
+};
+
+export const Slider: Story<Props> = ({ max, min, step, value, setValue }) => {
+  return (
+    <div>
+      <ReactSlider
+        className={styles.horizontalSlider}
+        marks={step}
+        markClassName={styles.mark}
+        min={min}
+        max={max}
+        step={step}
+        thumbClassName={styles.thumb}
+        trackClassName={styles.track}
+        value={value}
+        onChange={(value) => setValue(value)}
+        renderThumb={(props, state) => (
+          <div {...props}>
+            <div className={styles.toolTip}>
+              <div style={{ transform: "rotate(45deg)" }}>{state.valueNow}</div>
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
+
+Slider.defaultProps = {};
