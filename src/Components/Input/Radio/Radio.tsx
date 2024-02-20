fb2c08bb5fb11defe64e1f825e112b580b491ca9
donathany <61956007+donathany@users.noreply.github.com>
2024-02-20 14:55:56 +0800

    Feature/toggle switch (#3)
    * input initial commit
    
    Checkbox, Radio, Text inputs
    
    * slider initial commit
    
    slider initial commit + input resolutions
    
    * moved files
    
    moved Ladle/Inputs folder into Ladle/Components
    
    * working commit
    
    commit of radio, checkbox, and slider in a working state, with binded labels to check
    
    * slider update
    
    missing slider alignment, needs cleaning up
    
    * slider code-only
    
    90% done, math still not quite there
    
    * react-slider
    
    redid the whole slider component with react-slider
    
    * toggle switch
    
    2 way and 3 way toggle switch
    
    * style edits
    
    * toggle/radio update
    
    updated setChecked and handleChange.
    
    * merge
    
    merge main into feature/toggle-switch
    
    * toggle
    
    conslidated two-way and three-way toggle into generic multi-toggle. can mix and match text and icons.

--- src/Components/Input/Radio/Radio.tsx
+++ src/Components/Input/Radio/Radio.tsx
@@ -12,7 +12,6 @@ type Props = {
 
 export const Radio: Story<Props> = ({
   label,
-  checked,
   disabled,
   name,
   value,
@@ -24,8 +23,8 @@ export const Radio: Story<Props> = ({
       name={name}
       value={value}
       disabled={disabled}
-      onChange={() => {
-        setChecked(value);
+      onChange={(e) => {
+        setChecked(e.target.value);
       }}
     />
     <div className={styles.inputLabel}>{label}</div>
---
+++ src/Components/Toggle/ToggleSwitch/Toggle.tsx
@@ -0,0 +1,42 @@
+import type { Story } from "@ladle/react";
+import styles from "./Toggle.module.css";
+import { IconContext } from "react-icons";
+
+type Props = {
+  name: string;
+  values: string[];
+  labels?: any[];
+  iconSize?: string;
+  setChecked: (value: string) => void;
+};
+
+export const Toggle: Story<Props> = ({
+  values,
+  labels,
+  name,
+  setChecked,
+  iconSize = "24px",
+}) => {
+  return (
+    <div className={styles.container}>
+      {values?.map((values, index) => (
+        <label className={styles.switch}>
+          <input
+            type="radio"
+            name={name}
+            value={values}
+            onChange={(e) => {
+              setChecked(e.target.value);
+            }}
+          />
+
+          <IconContext.Provider value={{ size: `${iconSize}` }}>
+            <span className={`${styles.checkmark} ${styles.left}`}>
+              {labels ? labels[index] : values}
+            </span>
+          </IconContext.Provider>
+        </label>
+      ))}
+    </div>
+  );
+};
