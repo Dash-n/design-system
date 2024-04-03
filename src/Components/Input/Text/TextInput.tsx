56db9f30fc1eac132fb740994ec15245ca78dd21
donathany <donathany94@gmail.com>
2024-04-03 20:23:06 +0800

    Inputs

--- src/Components/Input/Text/TextInput.tsx
+++ src/Components/Input/Text/TextInput.tsx
@@ -1,5 +1,6 @@
 import type { Story } from "@ladle/react";
 import styles from "./TextInput.module.css";
+import { useState, useEffect, useRef } from "react";
 
 type Props = {
   id: string;
@@ -21,26 +22,44 @@ export const TextInput: Story<Props> = ({
   maxLength,
   size,
   updateValue,
-}) => (
-  <div className={styles.inputBox}>
-    <input
-      className={styles.input}
-      id={id}
-      name={name}
-      placeholder={placeholder}
-      disabled={disabled}
-      maxLength={maxLength}
-      size={size}
-      onKeyUp={updateValue}
-    />
-    {label && (
-      <label htmlFor={id} className={styles.label}>
-        {label}
-      </label>
-    )}
-  </div>
-);
+}) => {
+  const [content, setContent] = useState("");
+  const [width, setWidth] = useState(0);
+  const span = useRef();
 
+  // useEffect(() => {
+  //   setWidth(span.current.offsetWidth);
+  // }, [content]);
+
+  const changeHandler = (evt) => {
+    setContent(evt.target.value);
+  };
+
+  return (
+    <div className={styles.inputBox}>
+      <input
+        className={styles.input}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        disabled={disabled}
+        maxLength={maxLength}
+        size={size}
+        onKeyUp={updateValue}
+      />
+      {label && (
+        <label
+          htmlFor={id}
+          className={styles.label}
+          // ref={span}
+          // onChange={changeHandler}
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
 export const controlInput = TextInput.bind({});
 
 TextInput.args = {
