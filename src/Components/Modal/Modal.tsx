69f477ebee5ff92a47e9ec32aaea7ea5ecea82cc
donathany <donathany94@gmail.com>
2024-04-16 13:38:54 +0800

    feature/modal

---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import type { Story } from "@ladle/react";
+import styles from "./Modal.module.css";
+import { useState } from "react";
+
+type Props = {
+  children: React.ReactNode;
+  // modal: boolean;
+  // toggleModal: () => void;
+};
+
+export const Modal: Story<Props> = ({ children }) => {
+  const [modal, setModal] = useState(true);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <button onClick={toggleModal}>Open Modal</button>
+      {modal && (
+        <div className={styles.modal}>
+          <div className={styles.overlay} onClick={toggleModal} />
+
+          <div className={`${styles.modalContainer}`}>{children}</div>
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/Ladle/Toast/Modal.stories.tsx
@@ -0,0 +1,36 @@
+import type { Story } from "@ladle/react";
+import { Modal } from "../../Components/Modal/Modal.tsx";
+import { useState } from "react";
+
+type Props = {
+  content: any;
+  disabled?: boolean;
+  variant?: string;
+};
+
+const exampleContent = [
+  {
+    title: "Appointment Cancelled",
+    content: "Physio has cancelled your appointment",
+  },
+];
+
+export const Modals: Story<Props> = ({}) => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+  return (
+    <div>
+      {/* <button onClick={toggleModal}></button> */}
+      <div style={{ display: "flex", gap: "8px" }}>
+        <Modal
+          // modal={modal}
+          // toggleModal={toggleModal}
+          children={<p>Content</p>}
+        />
+      </div>
+    </div>
+  );
+};
