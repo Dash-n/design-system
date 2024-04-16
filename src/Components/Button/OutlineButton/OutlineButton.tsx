ed9a9ba00713fc65b8942549e6168ae6c4da127c
donathany <donathany94@gmail.com>
2024-04-16 13:39:14 +0800

    Update OutlineButton.tsx

--- src/Components/Button/OutlineButton/OutlineButton.tsx
+++ src/Components/Button/OutlineButton/OutlineButton.tsx
@@ -6,13 +6,21 @@ type Props = {
   label: string;
   disabled?: boolean;
   variant: string;
+  customStyles?: string;
+  onClick: () => void;
 };
 
-export const OutlineButton: Story<Props> = ({ label, variant, disabled }) => {
+export const OutlineButton: Story<Props> = ({
+  label,
+  variant,
+  disabled,
+  onClick,
+}) => {
   return (
     <button
-      className={`${styles[variant]} ${styles.button}`}
+      className={`${styles[variant]} ${styles.button} ${styles.customClass}`}
       disabled={disabled}
+      onClick={onClick}
     >
       {label}
     </button>
