07cbfa863a87c8ee675c120d4191e033ea5209e1
donathany <donathany94@gmail.com>
2024-04-26 11:53:50 +0800

    added converttodatetimelocalstring

--- src/Utils/toTitleCase.ts
+++ src/Utils/toTitleCase.ts
@@ -1,9 +1,11 @@
 export const toTitlecase = (label: string) => {
-  return label
-    .replace(/\w\S*/g, function (txt) {
-      return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
-    })
-    .replace(/_\w/g, function (txt) {
-      return " " + txt.substring(1).toUpperCase();
-    });
+  if (typeof label === "string") {
+    return label
+      .replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+      })
+      .replace(/_\w/g, function (txt) {
+        return " " + txt.substring(1).toUpperCase();
+      });
+  } else return label;
 };
