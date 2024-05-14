656f73224b05c326a5a70694e4486137a7e8be7d
Chris Chung <chrisjhchung@gmail.com>
2024-05-14 19:11:09 +1000

    chore: add github action and comment out some code

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,18 +1,17 @@
-import type { Story } from "@ladle/react";
-import styles from "./Toast.module.css";
-import { toTitlecase } from "../../../Utils/index.ts";
+// import type { Story } from "@ladle/react";
+// // import styles from "./Toast.module.css";
 
-type Props = {
-  label: string;
-  disabled?: boolean;
-  content: string;
-};
+// type Props = {
+//   label: string;
+//   disabled?: boolean;
+//   content: string;
+// };
 
-export const Toast: Story<Props> = ({ content }) => {
-  return (
-    <div className={styles.cardContainer}>
-      <div className={styles.cardTitle}>{title}</div>
-      <div className={styles.contentContainer}></div>
-    </div>
-  );
-};
+// export const Toast: Story<Props> = ({ content, label }) => {
+//   return (
+//     <div className={styles.cardContainer}>
+//       <div className={styles.cardTitle}>{label}</div>
+//       <div className={styles.contentContainer}></div>
+//     </div>
+//   );
+// };
